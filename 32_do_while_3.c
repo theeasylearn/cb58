eedd7524115c1cf84c817cd6d9e6c45adba23a87
theeasylearn <theeasylearn@gmail.com>
2026-03-05 19:15:58 +0530

    do while loop

--- 32_do_while_3.c
+++ 32_do_while_3.c
@@ -1,9 +1,30 @@
 /*
-    write a findout whether given number is perfect number not 
-    
+    write a findout whether given number is perfect number not
+    28 =  1 + 2 + 4 + 7 + 14 = 28
+    06 =  1 + 2 + 3  = 6
+    A Perfect Number is a positive number that is equal to the sum of its proper divisors (all divisors except the number itself).
 */
 #include <stdio.h>
 void main()
 {
-  
+    int number, reminder, sum = 1, divisor = 2;
+    printf("enter number");
+    scanf("%d", &number);
+    do
+    {
+        reminder = number % divisor; // 0
+        if (reminder == 0)
+        {
+            sum = sum + divisor; // 3
+        }
+        divisor = divisor + 1;
+    } while (divisor <= number / 2);
+    if (sum == number)
+    {
+        printf("%d is perfect number", number);
+    }
+    else
+    {
+        printf("%d is not perfect number", number);
+    }
 }
