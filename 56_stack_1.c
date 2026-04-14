1673991cd1927d33a5e88a039aaec6310b7003aa
The EasyLearn Academy <theeasylearn@users.noreply.github.com>
2026-04-14 07:22:56 +0530

    Update 56_stack_1.c

--- 56_stack_1.c
+++ 56_stack_1.c
@@ -1,8 +1,9 @@
 // #stack using array in c language
 #include <stdio.h>
+#define SIZE 5
 void main()
 {
-    int choice = 0;
+    int choice = 0, top = -1, stack[SIZE], item;
     do
     {
         printf("\nPress 1 to insert new item (push)");
@@ -18,19 +19,51 @@ void main()
         }
         else if (choice == 1)
         {
-            printf("push");
+            if (top > SIZE)
+            {
+                printf("\nstack overflow");
+            }
+            else
+            {
+                top++;
+                printf("Enter value to push into stack");
+                scanf("%d", &item); // 10
+                stack[top] = item;
+                printf("item pushed");
+            }
         }
         else if (choice == 2)
         {
-            printf("pop");
+            if (top < 0)
+            {
+                printf("stack is empty (underflow)");
+            }
+            else
+            {
+                printf("\n value remove from stack %d", stack[top]);
+                stack[top] = 0;
+                top--;
+            }
         }
         else if (choice == 3)
         {
             printf("display all");
+            for (int i = top; i >= 0; i--) // 3
+            {
+                printf("\n %d", stack[i]);
+            }
         }
         else if (choice == 4)
         {
             printf("display top most item");
+            if (top == -1)
+            {
+                printf("\n stack is empty");
+            }
+            else
+            {
+                printf("top most value in stack %d", stack[top]);
+            }
         }
         else
         {
